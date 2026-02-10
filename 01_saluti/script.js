6b03fce12b299bea13793bb49a904824bde88ac3
angeloren92 <angeloren92@gmail.com>
2026-02-10 15:01:51 +0100

    sol snack1

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -2,12 +2,29 @@
 
 const userName = 'Mario';
 
-
 // Dichiara la funzione qui.
+/**
+ * ## Hello World
+ * @param {string} nome - inserisci un nome 
+ * @returns restituisce un saluto + nome
+ */
+function helloWorld (nome) {
+    return `Ciao ${nome} `;
+}
 
+//arrow function
+const helloWorld2 = name => `Ciao ${name}`
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const saluti = helloWorld(userName);
+console.log(saluti);
+//arrow function log
+console.log(helloWorld2(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+const saluti2 = helloWorld('Mario');
+console.log(saluti2);
+//arrow function log
+console.log(helloWorld2('Mario'));
