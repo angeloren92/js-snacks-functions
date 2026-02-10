949db515cf54d1d09deb20721ffe9f39c0cf775d
angeloren92 <angeloren92@gmail.com>
2026-02-10 17:12:53 +0100

    sol snack5

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -7,12 +7,46 @@ e buonasera se è sera (oltre le 17)
 
 const name = 'Mario';
 
-
 // Dichiara la funzione qui.
+/**
+ * ## HelloWorld formale in base all'orario
+ * @param {string} string -inserisci un nome
+ * @returns restituisce un saluto formale con il nome da salutare
+ */
+function helloWorld (string) {
+    let text;
+    const time = new Date();
+    const hh = time.getHours();
 
+    if (hh < 13) {
+        text = "Buongiorno, ";
+    } else if (hh >= 13 && hh < 17) {
+        text = "Buon Pomeriggio, ";
+    } else {
+        text = "Buonasera, ";
+    }
+    return text + string;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(helloWorld(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+
+
+//arrow function
+const helloWorld2 = string => {
+    let text;
+    const time = new Date();
+    const hh = time.getHours();
+    if (hh < 13) {
+        text = "Buongiorno, ";
+    } else if (hh >= 13 && hh < 17) {
+        text = "Buon Pomeriggio, ";
+    } else {
+        text = "Buonasera, ";
+    }
+    return text + string;
+}
+console.log(helloWorld2(name));
