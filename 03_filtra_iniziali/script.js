5e9e1f758fa9da1e7f43c39f9b5e2856a0dcce1c
angeloren92 <angeloren92@gmail.com>
2026-02-10 15:46:27 +0100

    sol snack3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,38 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+/**
+ * ## Find the words with the first letter
+ * @param {arrow} arr - inserisci una arrow con delle stringhe 
+ * @param {string} initial - inserisci la lettera-iniziale da ricercare nell'arrow
+ * @returns restituisce gli elementi dell'arrow che contengono come iniziale la lettera inserita
+ */
+function findWordsWithInitial (arr, initial) {
+    const wordsWithInitial = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i][0].toUpperCase() === initial.toUpperCase()) {
+        let wordWithInitial = arr[i];
+        wordsWithInitial.push(wordWithInitial);
+        }
+    }
+    return wordsWithInitial;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(findWordsWithInitial(names, 'a'));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+//arrow function
+const wordsWithInitial = (arr, initial) => {
+    const wordsWithInitial = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i][0].toUpperCase() === initial.toUpperCase()) {
+        let wordWithInitial = arr[i];
+        wordsWithInitial.push(wordWithInitial);
+        }
+    }
+    return wordsWithInitial;
+}
+console.log(wordsWithInitial(names, 'a'));
