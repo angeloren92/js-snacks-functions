b66cc23f899d71fc033ca305ad8ed76b0acc272f
angeloren92 <angeloren92@gmail.com>
2026-02-10 15:21:03 +0100

    sol snack2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,34 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+/**
+ * ## Trova l'iniziale
+ * @param {array} arr - inserisci un array con stringhe
+ * @returns restituisce la prima lettera di ogni elemento nell'array
+ */
+function getInitials (arr) {
+    const initials = [];
+    for (let i = 0 ; i < arr.length ; i++) {
+        let char = arr[i][0];
+        initials.push(char);
+    }
+    return initials;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(getInitials(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+//arrow function
+const initials = arr => {
+    const initials = [];
+    for (let i = 0 ; i < arr.length ; i++) {
+        let char = arr[i][0];
+        initials.push(char);
+    }
+    return initials;
+}
+console.log(initials(names))
